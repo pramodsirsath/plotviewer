8026f6b2a906613f8ea0c860b3132e91dcadc9fd
pramodsirsath <pramodsirsath1406@gmail.com>
2026-04-05 21:42:23 +0530

    Refactor DB connection and serverless handler
    Refactor MongoDB connection logic and server handler.

--- server/server.js
+++ server/server.js
@@ -4,17 +4,23 @@ const mongoose = require('mongoose');
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
     console.log('MongoDB Connected');
-
-    const PORT = process.env.PORT || 5000;
-    const HOST = process.env.HOST || '0.0.0.0';
-
-    app.listen(PORT, HOST, () => {
-      console.log(`Server running on http://${HOST}:${PORT}`);
-    });
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error('MongoDB Connection Error:', err);
-  });
+    throw err;
+  }
+};
+
+// Vercel serverless handler
+module.exports = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
